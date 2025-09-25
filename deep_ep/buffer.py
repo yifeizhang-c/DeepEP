a29bef760f4227fdc88173620e1c891176fff770
Yifei Zhang <219273404+yifeizhang-c@users.noreply.github.com>
2025-09-25 00:04:25 -0700

    Enlarge NVSHMEM_QP_DEPTH

--- deep_ep/buffer.py
+++ deep_ep/buffer.py
@@ -92,7 +92,8 @@ class Buffer:
             os.environ['NVSHMEM_IBGDA_NIC_HANDLER'] = 'gpu'
             os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] = f'{num_qps_per_rank}'
             # Make sure QP depth is always larger than the number of on-flight WRs, so that we can skip WQ slot check
-            os.environ['NVSHMEM_QP_DEPTH'] = '1024'
+            os.environ['NVSHMEM_QP_DEPTH'] = os.environ.get('NVSHMEM_QP_DEPTH', '1024')
+            print(f'NVSHMEM_QP_DEPTH: {os.environ['NVSHMEM_QP_DEPTH']}')
 
             # Reduce gpu memory usage
             # 6 default teams + 1 extra team
